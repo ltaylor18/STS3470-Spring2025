4c2c208926a174085ec315544227fdd080842acb
Laura Taylor <ltaylor18@elon.edu>
2025-03-10 11:51:24 -0400

    Notes 9 continued

---
+++ 17 - 2025_03_10 - Notes 9.R
@@ -0,0 +1,110 @@
+# Day 17 - 3/10/2025
+# Notes 9
+# Note: See Code to Copy file!
+
+#----------------------------------------------------
+
+#Example 4
+my_mat <- matrix(1:15,nrow=3)
+myrowmeans <- NULL
+for(i in 1:nrow(my_mat)){
+  myrowmeans[i] <- mean(my_mat[i,])
+}
+myrowmeans
+
+apply(my_mat,1,mean)
+
+#What if? We omit [i]
+myrowmeans <- NULL
+for(i in 1:nrow(my_mat)){
+  myrowmeans <- mean(my_mat[i,])
+}
+myrowmeans
+
+
+
+#Mean of each column update
+
+my_mat <- matrix(1:15,nrow=3)
+mycolmeans <- NULL
+for(i in 1:ncol(my_mat)){
+  mycolmeans[i] <- mean(my_mat[,i])
+}
+mycolmeans
+
+#------------------------------------------------
+mypvals <- NULL
+mycis <- matrix(NA,nrow=3,ncol=2)
+for(i in 1:3){
+  out <- t.test(randu[,i],mu=0.5)
+  mypvals[i] <- out$p.value
+  mycis[i,] <- out$conf.int
+}
+mypvals
+mycis
+
+out <- t.test(mtcars$mpg)
+out
+names(out)
+
+#Equivalent to:
+mypvals <- NULL
+mycis <- matrix(NA,nrow=3,ncol=2)
+
+#Iteration 1
+out <- t.test(randu[,1],mu=0.5)
+mypvals[1] <- out$p.value
+mycis[1,] <- out$conf.int
+
+#Iteration 2
+out <- t.test(randu[,2],mu=0.5)
+mypvals[2] <- out$p.value
+mycis[2,] <- out$conf.int
+
+#Iteration 3
+out <- t.test(randu[,3],mu=0.5)
+mypvals[3] <- out$p.value
+mycis[3,] <- out$conf.int
+
+mypvals
+mycis
+
+
+
+#Example 5
+mysequence <- seq(100,2,-2)
+for(i in mysequence){print(i/2)}
+
+
+# -----------------------------------------------------------
+# Lesson 9 - Activity 3 Reference Code
+# Do not use this until you have attempted first!
+
+n <- 10 #update for #7, 10, 11
+CIcapture <- NULL
+mypower <- NULL
+mytypeII <- NULL
+index <- c(1, 3, 5, 6, 8, 9, 11, 13)
+for(k in index){
+  out <- NULL
+  pvalsim <- NULL
+  CIsim <- matrix(NA,nrow=1000,2)
+  for(i in 1:1000){
+    out <- sample(boxhouses$rooms, n, replace=FALSE)
+    out2 <- t.test(out,mu=k)
+    pvalsim[i] <- out2$p.value
+    CIsim[i,] <- out2$conf.int
+  }
+  print(k)
+  print("Power")
+  mypower <- sum(pvalsim <= 0.05)/1000
+  print(mypower)
+  print("Type II Error")
+  mytypeII <- 1-(sum(pvalsim <= 0.05)/1000)
+  print(mytypeII)
+
+  print("Capture Rate")
+  CIcapture <- sum(CIsim[,1] <= 7.42 & CIsim[,2] >= 7.42)/1000
+  print(CIcapture)
+
+}
